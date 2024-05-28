60a01aa9cad6727c93b6168caac72acfe72eb846
Ken Graf <kengraf57@gmail.com>
2024-05-27 21:12:05 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-STACK_NAME=$1
-
-if [ -z "$1" ]
-  then
-    echo "No STACK_NAME argument supplied"
-    exit 1
-fi
-
-sed -ri "s/YOUR-DOMAIN-NAME/${STACK_NAME}/" parameters.json
-
-S3BUCKET=$STACK_NAME-$(tr -dc a-f0-9 </dev/urandom | head -c 10)
-S3BUCKET="pi-exam-f6cc2fe20f"
-sed -ri "s/YOUR-BUCKET-NAME/${S3BUCKET}/" parameters.json
+STACK_NAME=`jq -r 'map(select(.ParameterKey == "DeployName"))[0].ParameterValue' parameters.json`
+S3BUCKET=`jq -r 'map(select(.ParameterKey == "S3BucketName"))[0].ParameterValue' parameters.json`
 
 echo "Creating stack..."
 
 # upload lambda functions
-zip pi-exam.zip -xi pi-exam.py
-aws s3 cp pi-exam.zip s3://${S3BUCKET}/pi-exam.zip
+zip ${STACK_NAME}.zip -xi ${STACK_NAME}.py
+aws s3 cp ${STACK_NAME}.zip s3://${S3BUCKET}/${STACK_NAME}.zip
 
 # upload cf stack
 aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags file://tags.json --output text

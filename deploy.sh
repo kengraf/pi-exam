7217c64650c0ac7f991fc12132149981af309a22
Ken Graf <kengraf57@gmail.com>
2024-05-27 21:26:18 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ zip ${STACK_NAME}.zip -xi ${STACK_NAME}.py
 aws s3 cp ${STACK_NAME}.zip s3://${S3BUCKET}/${STACK_NAME}.zip
 
 # upload cf stack
-aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags file://tags.json --output text
+aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags Key=ProjectName,Value=${STACK_NAME}  --output text
 
 echo "Waiting on ${STACK_NAME} create completion..."
 aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}

795a0900e2c1716c082370f3f26d14f40a13d7ad
Ken Graf <kengraf57@gmail.com>
2024-05-28 08:24:51 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-STACK_NAME=`jq -r 'map(select(.ParameterKey == "DeployName"))[0].ParameterValue' parameters.json`
+DEPLOY_NAME=`jq -r 'map(select(.ParameterKey == "DeployName"))[0].ParameterValue' parameters.json`
 S3BUCKET=`jq -r 'map(select(.ParameterKey == "S3bucketName"))[0].ParameterValue' parameters.json`
 
-echo "Creating stack..."
+echo "Creating stack...${DEPLOY_NAME}"
 
 # upload lambda functions
-zip ${STACK_NAME}.zip -xi ${STACK_NAME}.py
-aws s3 cp ${STACK_NAME}.zip s3://${S3BUCKET}/${STACK_NAME}.zip
+zip ${DEPLOY_NAME}.zip -xi ${DEPLOY_NAME}.py
+aws s3 cp ${DEPLOY_NAME}.zip s3://${S3BUCKET}/${DEPLOY_NAME}.zip
 
 # upload cf stack
-aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags Key=ProjectName,Value=${STACK_NAME}  --output text
+aws cloudformation create-stack --stack-name ${DEPLOY_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags Key=ProjectName,Value=${DEPLOY_NAME}  --output text
 
-echo "Waiting on ${STACK_NAME} create completion..."
-aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
-aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
+echo "Waiting on ${DEPLOY_NAME} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${DEPLOY_NAME}
+aws cloudformation describe-stacks --stack-name ${DEPLOY_NAME} | jq .Stacks[0].Outputs

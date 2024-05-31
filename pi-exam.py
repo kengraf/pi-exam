85e948bf47d457cea75ef3d7cd23bef6b7cb6470
Ken Graf <kengraf57@gmail.com>
2024-05-31 18:23:47 -0400

    Update pi-exam.py

--- pi-exam.py
+++ pi-exam.py
@@ -1,26 +1,52 @@
 import json
 
+request_state = 0
+response_body = ''
+
+def load_cookie_state(cookie):
+    # Extract the 'Cookie' header from the request
+    cookies = event['headers'].get('cookie', '')
+
+    # Split the cookies string into individual cookies
+    cookie_dict = {}
+    if cookies:
+        for cookie in cookies.split('; '):
+            name, value = cookie.split('=', 1)
+            cookie_dict[name] = value
+
+    # Get the value of 'my_cookie'
+    request_state = cookie_dict.get('state',0)
+    return 0
+
+def add_response_image( image_url ):
+    return 0
+
+def add_response_message( message ):
+    response_body += message
+    return 0
+    
 def lambda_handler(event, context):
     # Define the cookie attributes
-    cookie_value = 'my_cookie_value'
-    cookie_name = 'my_cookie'
-    max_age = 3600  # 1 hour in seconds
-    path = '/'
-    secure = True
-    http_only = True
+    load_cookie_state(event['headers'].get('cookie', ''))
 
+    # / 
+    # parm is pi = "yeah you know how to google pi" page success 
+    # pi by reducing digits PARMS DIGITS<100 =  success 0x01
+    # parm is SQLi = success 0X02
+    # AUTH==TRUE header  = success 0X04
+    # state cookie delta = success 0X08
+    # show "login" page
+    # /admin page
+    #    Direct request, return this code as HTML 0X08
+    # /champion page
     # Create the Set-Cookie header
-    set_cookie_header = f"{cookie_name}={cookie_value}; Max-Age={max_age}; Path={path};"
-    if secure:
-        set_cookie_header += ' Secure;'
-    if http_only:
-        set_cookie_header += ' HttpOnly;'
+    cookie_header = f"state={state}; Max-Age=3600; Path=/; Secure; HttpOnly;"
 
     # Create the response
     response = {
         'statusCode': 200,
         'headers': {
-            'Set-Cookie': set_cookie_header,
+            'Set-Cookie': cookie_header,
             'Content-Type': 'application/json'
         },
         'body': json.dumps({'message': 'Cookie has been set!'})
